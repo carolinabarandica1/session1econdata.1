e23abd40a44efbd3cabbd0e67e4a9f5016e012a4
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-12 18:50:51 +0100

    new commit

--- session 1.2.py
+++ session 1.2.py
@@ -1,7 +1,5 @@
 import random
-
 from point import Point
-
 
 class ColorPoint(Point):
     def __init__(self, x, y, color):
@@ -11,17 +9,17 @@ class ColorPoint(Point):
     def __str__(self):
         return f"<{self.color}: {self.x}, {self.y}>"
 
-
 p = ColorPoint(1, 2, "red")
 print(p)
 
-colors = ['red','green','blue','yellow','black','magenta','cyan','white','burgundy','periwinkle','marsala']
+colors = ['red', 'green', 'blue', 'yellow', 'black', 'magenta', 'cyan', 'white', 'burgundy', 'periwinkle', 'marsala']
 
 color_points = []
-for i in range (18):
-    color_points.append(ColorPoint(random.randint(-10,10)), random.randint(-10,10),
-                        random.choice(colors))
+for i in range(18):
+    color_points.append(ColorPoint(random.randint(-10, 10), random.randint(-10, 10), random.choice(colors)))
 
 print(color_points)
-color_points.sort()
+
+color_points.sort(key=lambda p: (p.x**2 + p.y**2))
+
 print(color_points)
