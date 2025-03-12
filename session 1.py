2ed3f3a1c6742baa8c91498cafe001abb48016bf
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-12 17:42:52 +0100

    new commit

--- session 1.py
+++ session 1.py
@@ -1,22 +1,34 @@
+import random
+
 class Point:
     def __init__(self, x, y):
-
         """
-        initialize a point object.
+        Initialize a point object.
         :param x: the x position on the axis
         :param y: the y position on the axis
         """
-
         self.x = x
         self.y = y
 
-p = Point(1,2)
-p2 = Point(2,3)
-p4 = Point(4.4,-55)
+    def __repr__(self):
+        return f'Point({self.x}, {self.y})'
+
+p = Point(1, 2)
+p2 = Point(2, 3)
+p4 = Point(4.4, -55)
 
 print(f'p.x={p.x} and p.y={p.y}')
-print(f'p4.x={p4.x} and p4, y = {p4.y}')
+print(f'p4.x={p4.x} and p4.y={p4.y}')
 p.x = 20
 
 print(f'p.x={p.x} and p.y={p.y}')
 print(p)
+
+
+points = []
+for i in range(5):
+    points.append(Point(random.randint(-10, 10), random.randint(-10, 10)))
+
+print("I got these 5 random points:")
+for p in points:
+    print(p)
