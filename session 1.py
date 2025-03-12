57e78096c52f45b0ef3045f076ddacb206df6b6e
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-12 17:09:49 +0100

    new commit

--- session 1.py
+++ session 1.py
@@ -9,3 +9,13 @@ class Point:
 
         self.x = x
         self.y = y
+
+p = Point(1,2)
+p2 = Point(2,3)
+p4 = Point(4.4,-55)
+
+print(f'p.x={p.x} and p.y={p.y}')
+print(f'p4.x={p4.x} and p4, y = {p4.y}')
+p.x = 20
+
+print(f'p.x={p.x} and p.y={p.y}')
