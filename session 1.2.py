fe2cf392170e0d23d9aae6d23e9b388407e2ed37
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-12 18:44:45 +0100

    new commit

---
+++ session 1.2.py
@@ -0,0 +1,16 @@
+from point import Point
+
+
+class ColorPoint(Point):
+    def __init__(self, x, y, color):
+        super().__init__(x, y)
+        self.color = color
+
+    def __str__(self):
+        return f"<{self.color}: {self.x}, {self.y}>"
+
+
+p = ColorPoint(1, 2, "red")
+print(p)
+
+colors = ['red','green','blue','yellow','black']
