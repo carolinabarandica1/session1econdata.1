43d86bab9b795ff754666c563b8d7b185309f057
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-12 17:04:12 +0100

    Initial commit

---
+++ session 1.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x, y):
+
+        """
+        initialize a point object.
+        :param x: the x position on the axis
+        :param y: the y position on the axis
+        """
+
+        self.x = x
+        self.y = y
