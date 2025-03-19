e3d05463112781883a2543fa60af0c5de6da6cf6
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-19 16:56:45 +0100

    new commit

--- advances_point.py
+++ advances_point.py
@@ -1,7 +1,21 @@
 from session1_2 import ColorPoint
 
+
 class AdvancedPoint(ColorPoint):
+    COLORS = ['red', 'green', 'blue', 'yellow', 'black', 'magenta', 'cyan', 'white', 'burgundy', 'periwinkle',
+              'marsala']
+
+    def __init__(self, x, y, color):
+        if color not in self.COLORS:
+            raise PointException(f'Invalid color, must be one of {self.COLORS}')
+        super().__init__(x, y, color)
+
+
+class PointException(Exception):
     pass
-p = AdvancedPoint(1,2,"blue")
+
+
+p = AdvancedPoint(1, 2, "blue")
 print(p)
 print(p.distance_orig())
+
