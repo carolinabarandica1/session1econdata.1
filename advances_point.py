2b0f40ca204eac0d0fc7bf3b0ee720c2668ea5fe
carolinabarandica <carolinabarandica1@gmail.com>
2025-03-19 17:06:55 +0100

    new commit

--- advances_point.py
+++ advances_point.py
@@ -1,5 +1,7 @@
 from session1_2 import ColorPoint
 
+class PointException(Exception):
+    pass
 
 class AdvancedPoint(ColorPoint):
     COLORS = ['red', 'green', 'blue', 'yellow', 'black', 'magenta', 'cyan', 'white', 'burgundy', 'periwinkle',
@@ -10,18 +12,24 @@ class AdvancedPoint(ColorPoint):
             raise PointException(f'Invalid color, must be one of {self.COLORS}')
         super().__init__(x, y, color)
 
-@classmethod
-def add_color(cls,color):
-    '''
-    add a new valid color for our class
-    '''
+    @classmethod
+    def add_color(cls, color):
+        '''
+        add a new valid color for our class
+        '''
+        cls.COLORS.append(color)
 
-    cls.COLORS.append(color)
-class PointException(Exception):
-    pass
+    @staticmethod
+    def from_tuple(coordinate,color ='red'):
+        '''
+        create a new point from a tuple rather than 2 individual viewsa
+        '''
+        x,y = coordinate
+        return AdvancedPoint(x,y,color)
 
-
+AdvancedPoint.add_color("rojo")
 p = AdvancedPoint(1, 2, "blue")
 print(p)
 print(p.distance_orig())
+
 
